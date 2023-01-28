1a47a4e80f9b004bca67b3bd9c9bae2ded19978f
Anshika Srivastava <75130949+AnshikaSrivastava25@users.noreply.github.com>
2023-01-28 20:02:27 +0530

    whats your name

--- whats_your_name.py
+++ whats_your_name.py
@@ -34,14 +34,14 @@ Explanation 0
 The input read by the program is stored as a string data type. A string is a collection of characters.
 """
 
-#
+
 # Complete the 'print_full_name' function below.
 #
 # The function is expected to return a STRING.
 # The function accepts following parameters:
 #  1. STRING first
 #  2. STRING last
-#
+
 
 def print_full_name(first, last):
     # Write your code here
